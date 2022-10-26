22e30129643327b222eff8126bcecc55fd2dbabd
(cdep)illabout <cdep.illabout@gmail.com>
2022-10-25 23:43:02 -0400

    Update nixpkgs pin.

--- flake.nix
+++ flake.nix
@@ -1,19 +1,14 @@
 {
   description = "Tool for building PureScript projects with Nix";
 
-  # The Nixpkgs checkout needs to include
-  # https://github.com/NixOS/nixpkgs/pull/144076, or commit
-  # bcfed07a3d30470143a2cae4c55ab952495ffe2f because that
-  # code is used in purescript2nix.  This should be a version of Nixpkgs master
-  # after 2021-12-08 or so.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
       # System types to support.
       supportedSystems = [
         "x86_64-linux"
-        "x86_64-darwin"
+        # "x86_64-darwin"
       ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
@@ -30,9 +25,7 @@
       overlay = import ./nix/overlay.nix;
 
       packages = forAllSystems (system: {
-        # This is just an example purescript package that has been built using
-        # the purescript2nix function.
-        inherit (nixpkgsFor.${system}) example-purescript-package;
+        inherit (nixpkgsFor.${system}) example-haskell-package;
       });
 
       # defaultPackage = forAllSystems (system: self.packages.${system}.hello);
