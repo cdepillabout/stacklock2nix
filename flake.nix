96d3a67f64ec7ed96a3a6c24c26135a0a3c6dcbf
(cdep)illabout <cdep.illabout@gmail.com>
2022-11-27 04:23:02 -0500

    no nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   description = "Nix library for generating a Nixpkgs Haskell package set from a stack.yaml.lock file";
 
-  # This is just pinned for CI.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
     # A Nixpkgs overlay.  This contains the stacklock2nix function that
     # end-users will want to use.
     overlay = import ./nix/overlay.nix;
