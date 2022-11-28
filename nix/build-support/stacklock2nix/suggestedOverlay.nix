65d95905dec287d82621db0be961dc6c418959e8
Dennis Gosnell <cdep.illabout@gmail.com>
2022-11-29 07:13:53 +0900

    Add override for tasty-discover

--- nix/build-support/stacklock2nix/suggestedOverlay.nix
+++ nix/build-support/stacklock2nix/suggestedOverlay.nix
@@ -31,15 +31,24 @@
 
 hfinal: hprev: with haskell.lib.compose; {
   HUnit = dontCheck hprev.HUnit;
+
   ansi-terminal = dontCheck hprev.ansi-terminal;
+
   async = dontCheck hprev.async;
+
   base-orphans = dontCheck hprev.base-orphans;
+
   # doctests fail
   bsb-http-chunked = dontCheck hprev.bsb-http-chunked;
+
   clock = dontCheck hprev.clock;
+
   colour = dontCheck hprev.colour;
+
   doctest = dontCheck hprev.doctest;
+
   doctest-parallel = dontCheck hprev.doctest-parallel;
+
   dyre =
     lib.pipe
       hprev.dyre
@@ -56,7 +65,9 @@ hfinal: hprev: with haskell.lib.compose; {
         # dyre's tests appear to be trying to directly call GHC.
         dontCheck
       ];
+
   focuslist = dontCheck hprev.focuslist;
+
   glib =
     lib.pipe
       hprev.glib
@@ -64,34 +75,67 @@ hfinal: hprev: with haskell.lib.compose; {
         (addPkgconfigDepend pkgs.glib.dev)
         # (addBuildTool hfinal.gtk2hs-buildtools)
       ];
+
   hashable = dontCheck hprev.hashable;
+
   haskeline = dontCheck hprev.haskeline;
+
   # This propagates this to everything depending on haskell-gi-base
   haskell-gi-base = addBuildDepend pkgs.gobject-introspection hprev.haskell-gi-base;
+
   hourglass = dontCheck hprev.hourglass;
+
   # fails because tests don't expect a revised cabal file
   hpack = dontCheck hprev.hpack;
+
   hspec = dontCheck hprev.hspec;
+
   hspec-core = dontCheck hprev.hspec-core;
+
   # Due to tests restricting base in 0.8.0.0 release
   http-media = doJailbreak hprev.http-media;
+
   logging-facade = dontCheck hprev.logging-facade;
+
   logict = dontCheck hprev.logict;
+
   mockery = dontCheck hprev.mockery;
+
   nanospec = dontCheck hprev.nanospec;
+
   # test suite doesn't build
   nothunks = dontCheck hprev.nothunks;
+
   random = dontCheck hprev.random;
+
   # Disabling doctests.
   regex-tdfa = overrideCabal { testTarget = "regex-tdfa-unittest"; } hprev.regex-tdfa;
+
   smallcheck = dontCheck hprev.smallcheck;
+
   splitmix = dontCheck hprev.splitmix;
+
   syb = dontCheck hprev.syb;
+
   tasty = dontCheck hprev.tasty;
+
+  tasty-discover =
+    if hprev.tasty-discover.version == "4.2.2" then
+      # Test suite is missing an import from hspec
+      # https://github.com/haskell-works/tasty-discover/issues/9
+      # https://github.com/commercialhaskell/stackage/issues/6584#issuecomment-1326522815
+      dontCheck hprev.tasty-discover
+    else
+      hprev.tasty-discover;
+
   tasty-expected-failure = dontCheck hprev.tasty-expected-failure;
+
   test-framework = dontCheck hprev.test-framework;
+
   unagi-chan = dontCheck hprev.unagi-chan;
+
   vector = dontCheck hprev.vector;
+
   # http://hydra.cryp.to/build/501073/nixlog/5/raw
   warp = dontCheck hprev.warp;
 }
