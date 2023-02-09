e9af3b818722d7742c89aaaa0b2cb27bd1c08c7f
Dennis Gosnell <cdep.illabout@gmail.com>
2023-02-09 21:24:51 +0900

    Add tests for the newPkgSet feature

---
+++ test/test-new-package-set.nix
@@ -0,0 +1,43 @@
+
+{ stacklock2nix
+, haskell
+, cabal-install
+, fetchurl
+}:
+
+let
+  hasklib = haskell.lib.compose;
+
+  stacklock = stacklock2nix {
+    stackYaml = ../my-example-haskell-lib-advanced/stack.yaml;
+    baseHaskellPkgSet = haskell.packages.ghc924;
+    additionalHaskellPkgSetOverrides = hfinal: hprev: {
+      # The servant-cassava.cabal file is malformed on GitHub:
+      # https://github.com/haskell-servant/servant-cassava/pull/29
+      servant-cassava =
+        hasklib.overrideCabal
+          { editedCabalFile = null; revision = null; }
+          hprev.servant-cassava;
+
+      amazonka = hasklib.dontCheck hprev.amazonka;
+      amazonka-core = hasklib.dontCheck hprev.amazonka-core;
+      amazonka-sso = hasklib.dontCheck hprev.amazonka-sso;
+      amazonka-sts = hasklib.dontCheck hprev.amazonka-sts;
+    };
+    cabal2nixArgsOverrides = args: args // {
+      amazonka-sso = ver: { amazonka-test = null; };
+      amazonka-sts = ver: { amazonka-test = null; };
+    };
+    additionalDevShellNativeBuildInputs = stacklockHaskellPkgSet: [
+      cabal-install
+    ];
+    all-cabal-hashes = fetchurl {
+      name = "all-cabal-hashes";
+      url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/9ab160f48cb535719783bc43c0fbf33e6d52fa99.tar.gz";
+      sha256 = "sha256-QC07T3MEm9LIMRpxIq3Pnqul60r7FpAdope6S62sEX8=";
+    };
+  };
+in
+[ stacklock.newPkgSet.my-example-haskell-app
+  stacklock.newPkgSetDevShell
+]
