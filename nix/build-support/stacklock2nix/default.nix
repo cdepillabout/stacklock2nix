739a50d0f62eee15a0967c351866732c64337bf2
Mr-Andersen <Mr-Andersen+github@cabal.run>
2026-01-28 18:08:18 +0400

    Add comment

--- nix/build-support/stacklock2nix/default.nix
+++ nix/build-support/stacklock2nix/default.nix
@@ -520,6 +520,7 @@ let
           mkdir -p "$out"
           cd "$out"
           ${lib.concatMapStrings copyForPkg pkgs}
+          # the derivation must not fail in case the very last `copyForPkg` fails
           true
         '';
 
